c7202687834799b3086a79a112d1a1f630520e2d
Koki Noda <sp.r2.11hd@gmail.com>
2022-08-16 12:18:08 +0900

    add GenCAT datasets

--- scripts/run_gencat.py
+++ scripts/run_gencat.py
@@ -106,7 +106,8 @@ def main(args):
         # parameters to be changed: n(node), m(edge)
         data_dir = f"{top_dir}/data/GenCAT_Exp_scalability_edge/"
         os.makedirs(data_dir, exist_ok=True)
-        params = [5000, 10000, 15000, 20000]  # change edge
+        # params = [5000, 10000, 15000, 20000, 25000]  # change edge
+        params = [25000]  # change edge
         n = len(node_degree)
         for m in params:
             for i in range(n_iter):
@@ -128,9 +129,10 @@ def main(args):
         # parameters to be changed: n(node), m(edge)
         data_dir = f"{top_dir}/data/GenCAT_Exp_scalability_node_edge/"
         os.makedirs(data_dir, exist_ok=True)
-        node_params = [3000, 6000, 9000, 12000]
-        edge_params = [5000, 10000, 15000, 20000]
-
+        # node_params = [3000, 6000, 9000, 12000, 15000, 60000, 120000]
+        # edge_params = [5000, 10000, 15000, 20000, 25000, 100000, 200000]
+        node_params = [60000, 120000]
+        edge_params = [100000, 200000]
         for i in range(len(node_params)):
             for j in range(n_iter):
                 adj, features, labels = gencat.gencat(
@@ -168,7 +170,7 @@ def main(args):
                 random.shuffle(class_size)
             return class_size
 
-        alpha_list = [0.5, 0.7, "flat"]
+        alpha_list = [0.4, 0.5, 0.6, 0.7, "flat"]
 
         for alpha in alpha_list:
             class_size_ = get_class_size(alpha)
